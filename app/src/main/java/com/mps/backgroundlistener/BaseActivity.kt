5c88c4533228f53986362fee068c5da61625f9ae
msp <congduan970818@163.com>
2025-11-20 09:57:34 +0800

    fix: 优化

--- app/src/main/java/com/mps/backgroundlistener/BaseActivity.kt
+++ app/src/main/java/com/mps/backgroundlistener/BaseActivity.kt
@@ -1,28 +1,36 @@
 package com.mps.backgroundlistener
 
-import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import kotlin.math.log
 
 /**
  * @description My Application2
  * @author msp
  * @time 2021/11/12
  */
-open class BaseActivity:AppCompatActivity() {
+open class BaseActivity : AppCompatActivity() {
 
     protected var isOpenPassword = true
     protected var isLogin = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        BackgroundListener.get(this).addListener(object :BackgroundListener.Listener{
+        BackgroundListener.get(this).addListener(object : BackgroundListener.Listener {
             override fun onBecameForeground() {
                 // 开启了 密码验证 并且 已经登录了
                 if (isOpenPassword && isLogin) {
-                    Log.e("BaseActivity", "该显示弹窗了嗷")
+                    AlertDialog.Builder(this@BaseActivity)
+                        .setCancelable(false)
+                        .setMessage("这里应该弹出一个Activity 或者 全局的 Dialog 只有输入密码后，才能继续使用app")
+                        .setPositiveButton("确定") { dialog, which ->
+                            dialog.dismiss()
+                        }
+                        .setNeutralButton("取消") { dialog, which ->
+                            dialog.dismiss()
+                        }
+                        .show()
                 }
                 Log.e("BaseActivity", "onBecameForeground:前台")
             }
